a9e5c2d8ad1349cea48b297c13d08e0699681ead
sebastianj1w <sebastianj1w@outlook.com>
2019-04-12 15:36:57 +0800

    disabled csrf

--- src/main/java/wordladder/WebSecurityConfig.java
+++ src/main/java/wordladder/WebSecurityConfig.java
@@ -15,6 +15,8 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.csrf().disable();
         http
                 .authorizeRequests()
                 .antMatchers("/word_ladder/**").permitAll()
