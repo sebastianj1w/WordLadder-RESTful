63d30731036a82e8edcfeac2d865abab505682e3
sebastianj1w <sebastianj1w@outlook.com>
2019-04-14 19:52:21 +0800

    update readme

--- src/main/java/wordladder/WebSecurityConfig.java
+++ src/main/java/wordladder/WebSecurityConfig.java
@@ -2,7 +2,9 @@ package wordladder;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -16,12 +18,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
-        http.csrf().disable().authorizeRequests()
-                .antMatchers("/word_ladder/**").permitAll()
-                .antMatchers("/actuator/**").permitAll()
-                .anyRequest().authenticated()
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/actuator/**").authenticated()
+//                .anyRequest().permitAll()
                 .and().formLogin().loginProcessingUrl("/login").permitAll()
                 .and().logout().logoutSuccessUrl("/").permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().mvcMatchers(HttpMethod.GET, "/word_ladder/**");
     }
 
     @Bean
