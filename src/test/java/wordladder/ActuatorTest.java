befbf4600d9a47c0ad7a5364a5bac4004b5825ae
sebastianj1w <sebastianj1w@outlook.com>
2019-04-14 20:10:53 +0800

    added actuator tests

---
+++ src/test/java/wordladder/ActuatorTest.java
@@ -0,0 +1,42 @@
+package wordladder;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+//@ContextConfiguration
+//@WithMockUser
+public class ActuatorTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Before
+    public void before() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac)
+                .dispatchOptions(true)
+                .build();
+    }
+
+    @Test
+    public void accessActuatorOk() throws Exception {
+        mockMvc.perform(get("/actuator"))
+                .andExpect(status().isOk());
+    }
+}
